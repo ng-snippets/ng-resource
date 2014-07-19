216c779eb08e535412ce79a485a22dc124195da1
Vamshi Krishna <pvamshi@gmail.com>
2014-07-19 07:55:57 +0530

    added resource logic

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,13 +1,32 @@
 'use strict';
 
 angular.module('tempAngularApp')
-  .controller('MainCtrl', function ($scope,$http) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-    $http.get('/api/v1/contacts').success(function(data){
-        console.log(data);
+    .controller('MainCtrl', function($scope, $resource, $log, $http) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+        var Contact = $resource('/api/:version/contacts/:id', {
+            version: 'v1',
+            id: '@id'
+        });
+        $scope.fetch_contacts = function() {
+            var contacts = Contact.query(function() {
+                $log.info(contacts);
+            });
+        };
+        $scope.add_contact = function() {
+            var newContact = new Contact({
+                'name': 'Vamshi',
+                'phone': '1001001001',
+                'email': 'vamshi@example.com'
+            });
+            newContact.name = 'Vamshi Krishna';
+            newContact.$save();
+        };
+        $http.get('todos.json')
+            .then(function(res) {
+                $scope.todos = res.data;
+            });
     });
-  });
